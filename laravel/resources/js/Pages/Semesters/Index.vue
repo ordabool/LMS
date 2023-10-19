632f822bda4a24089ebb98b31254fc72256fa27f
Or Dabool <or.dabool@gmail.com>
2023-10-19 12:06:58 +0000

    Created a semester model, and a vue page for it

---
+++ laravel/resources/js/Pages/Semesters/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Semesters" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <h1>Hello</h1>
+        </div>
+    </AuthenticatedLayout>
+</template>
