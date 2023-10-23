88752e1d403840efc670992a864a944790cc73f9
Or Dabool <or.dabool@gmail.com>
2023-10-23 10:59:31 +0300

    LMS-14: Added PrimeVue and PrimeIcon support

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,10 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import 'primeicons/primeicons.css';
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import PrimeVue from 'primevue/config';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -15,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(PrimeVue)
             .mount(el);
     },
     progress: {
